24f4960058389c3792f228c927560a652f965f4f
Samatchayaa <151843443+Phatcharaphon123@users.noreply.github.com>
2024-03-01 12:50:42 +0700

    33

--- 820/SERVER/index.js
+++ 820/SERVER/index.js
@@ -1,21 +1,93 @@
-//ทำการ import http เข้ามาเพือทำการรัน server
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-//กำหนด  host และ port เริ่มต้น
-const host = 'localhost'
-const port = 8000
+app.use(bodyParser.json());
 
-//กำหนด ค่าเริ่มต้นของ server เมื่อเปิดหน้าเว็บที่ localhost:8000 ขึ้นมา
-const requirelistener = function(req, res) {
-    res.writeHead(200)
-    res.end('My first server!');
-}  
+const port = 8000;
+// สำหรับเก็บ users
+let users = []
+let counter = 1;
 
-//ทำการ run server
-const server = http.createServer(requirelistener)
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)})
+/*
+1.	GET /users สำหรับ get users ทั้งหมดที่บันทึกเข้าไปออกมา
+2.	POST /users สำหรับการสร้าง users ใหม่บันทึกเข้าไป
+3.	GET /users/:id สำหรับการดึง users รายคนออกมา
+4.	PUT /users/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+5.	DELETE /users/:id สำหรับการลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+*/
+
+ // path = GET /users สำหรับ get users ทั้งหมดที่บันทึกเข้าไปออกมา
+app.get('/users', (req, res) => {
+  const filterUsers = users.map(user => {
+    return {
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      fullname: user.firstname + ' ' + user.lastname
+    }
+  })
+  res.json(filterUsers);
+})
+
+// path = POST /users สำหรับการสร้าง users ใหม่บันทึกเข้าไป
+app.post('/users', (req, res) => {
+  let user = req.body;
+  user.id = counter
+  counter += 1
+  users.push(user);
+  res.json({
+    message: 'Add new user successfully',
+    user: user
+  });
+})
+
+// path = GET /users/:id สำหรับการดึง users รายคนออกมา
+app.get('/users/:id', (req, res) => {
+  let id = req.params.id
+
+// หา index
+  let seletedIndex = users.findIndex(user => user.id == id)
+
+  res.json(users[seletedIndex]);
+})
 
 
+//path = PUT /users/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.put('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let updateUser = req.body;
+  // ค้นหาข้อมูล users
+  let seletedIndex = users.findIndex(user => user.id == id)
 
+  // update ข้อมูล user (null || 'ทดสอบ')
+    users[seletedIndex].firstname = updateUser.firstname || users[seletedIndex].firstname
+    users[seletedIndex].lastname = updateUser.lastname || users[seletedIndex].lastname
+    users[seletedIndex].age = updateUser.age || users[seletedIndex].age
+    users[seletedIndex].gender = updateUser.gender || users[seletedIndex].gender
 
+  res.json({
+    message: 'Update user successfully',
+    data:{
+      user: updateUser,
+      indexUpdate: seletedIndex
+    }
+  })
+})
+
+// path = DELETE /users/:id สำหรับการลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.delete('/users/:id', (req, res) => {
+  let id = req.params.id
+  //หาก่อนว่า index ของ user ที่ต้องการลบอยู่ที่ index ไหน
+  let seletedIndex = users.findIndex(user => user.id == id)
+  //ลบ users ออก
+  users.splice(seletedIndex, 1)
+  res.json({
+    message: 'Delete user successfully',
+      indexDelete: seletedIndex
+  })
+})
+
+app.listen(port, (req, res) => {
+    console.log('http server running on', + port);
+})
